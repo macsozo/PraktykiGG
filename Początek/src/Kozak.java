ae55a37280bd66f3c59e19671e35c92c14ce23df
macsozo <macsozot@gmail.com>
2021-02-23 14:45:35 +0100

    jeszcze nie skonczylem zadania 1

---
+++ Początek/src/Kozak.java
@@ -0,0 +1,11 @@
+public class Kozak
+{//wypisz text ktory zawiera wszystkie poznane zmienne (bez boolean).
+     int a;
+     double b;
+     char c;
+     String d;
+     System.out.println();
+     //napisz program, który oblicza sumę: a^2 + b^2. wypisz sam wynik
+     int
+
+}
