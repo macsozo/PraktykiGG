3474188c41bbd7e510ba1233a59932672e6b6a30
macsozo <macsozot@gmail.com>
2021-02-27 22:49:40 +0100

    zadanka

---
+++ Początek/src/zadanie8/Main.java
@@ -0,0 +1,19 @@
+package zadanie8;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int wysokoscTrojkata = 8;
+
+
+        for (int i = 0; i < wysokoscTrojkata; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print("X ");
+            }
+            System.out.println();
+        }
+    }
+}
