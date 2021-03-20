a5b734db2a6dcf6b370c28fe7d911d088367456b
macsozo <macsozot@gmail.com>
2021-03-20 16:02:25 +0100

    wersja 0.99

--- Początek/src/Projekt/Main.java
+++ Początek/src/Projekt/Main.java
@@ -9,27 +9,53 @@ public class Main {
         char[][] plansza = new char[wymiar][wymiar];
         int movesCounter = 0;
         char activePlayer = 'X';
+        String wygranaGracza1 = "wygrywa gracz 1";
+        String wygranaGracza2 = "wygrywa gracz 2";
+        boolean wygrana1 = false;
+        boolean wygrana2 = false;
 
-        while (true) {
-            printplansza(plansza);
-            boolean ruchpoprawny = wykonajruch(plansza, activePlayer);
-            if (ruchpoprawny) {
-                boolean wygranawiersze = sprawdzwiersze(plansza,activePlayer);
-                boolean wygranakolumny = sprawdzkolumny(plansza,activePlayer);
-                boolean wygranaskos1 = sprawdzskos1(plansza,activePlayer);
-                boolean wygranaskos2 = sprawdzskos2(plansza,activePlayer);
-        if(wygranawiersze|| wygranaskos1 || wygranaskos2 || wygranakolumny){
-            System.out.println("Gratulacje");
+        while (movesCounter < wymiar * wymiar) {
+            try {
+                printplansza(plansza);
+                boolean moveWasCorrect = performMove(plansza, activePlayer);
+                if (moveWasCorrect) {
 
-                activePlayer = activePlayer == 'X' ? 'O' : 'X';
-            } else {
-                System.out.println("Ruch niepoprawny, spróbuj ponownie");
+                    movesCounter++;
+
+                    activePlayer = activePlayer == 'X' ? 'O' : 'X';
+                } else {
+                    System.out.println("Ruch niepoprawny, spróbuj ponownie");
+                }
+
+            }catch(Exception exception)
+            {
+                System.out.println("błąd, źle napsisales");
+                continue;
             }
-        }
-        printplansza(plansza);
-        System.out.println("Koniec gry :)");
-    }
+            if(plansza[0][0]=='X' && plansza[0][1]=='X' && plansza[0][2]=='X' )// zrobic to 9 razy
+            {
+                wygrana1 = true;//wpisywanie danych
+            }
+             else if(plansza[0][0]=='O' && plansza[0][1]=='O' && plansza[0][2]=='O' )
+             {
+                 wygrana2 = true;
+             }
+//miejsce na dorabianie
+            if(wygrana1==true){// porownywanie danych
+                System.out.println(wygranaGracza1);
+                System.out.println("Gratulacje Gracz1");
+                break;
+            }
+           else if(wygrana2==true) {
+                System.out.println(wygranaGracza2);
+                break;
+            }
+            else if(movesCounter==wymiar*wymiar)
+                System.out.println("REMIS");
+            printplansza(plansza);
 
+        }
+    }
 
 
     private static boolean performMove(char[][] plansza, char activePlayer) {
@@ -50,20 +76,20 @@ public class Main {
     }
 
     private static void printplansza(char[][] plansza) {
-            int wymiar = plansza.length;
+        int wymiar = plansza.length;
 
-            System.out.print("\t");
-            for (int i = 0; i < wymiar; i++) {
-                System.out.print(i + "\t");
+        System.out.print("\t");
+        for (int i = 0; i < wymiar; i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+        for (int row = 0; row < wymiar; row++) {
+            System.out.print(row + ":\t");
+            for (int kolumna = 0; kolumna < wymiar; kolumna++) {
+                System.out.print(plansza[row][kolumna] + "\t");
             }
             System.out.println();
-            for (int row = 0; row < wymiar; row++) {
-                System.out.print(row + ":\t");
-                for (int kolumna = 0; kolumna < wymiar; kolumna++) {
-                    System.out.print(plansza[row][kolumna] + "\t");
-                }
-                System.out.println();
-            }
         }
     }
+}
 
